924cf99984b6bee2f6ce9323728062df33538af2
Fayorr <fayokunmiosho@gmail.com>
2026-02-04 18:21:19 +0100

    fixing render.com billing error/move to vercel

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,15 +3,37 @@ require('dotenv').config();
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp';
 
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectToDB = async () => {
-	await mongoose
-		.connect(mongoURI)
-		.then(() => {
+	if (cached.conn) {
+		return cached.conn;
+	}
+
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false, // Disable Mongoose buffering
+		};
+		console.log('Connecting to MongoDB...');
+		cached.promise = mongoose.connect(mongoURI, opts).then((mongoose) => {
 			console.log('Connected to MongoDB Successfully');
-		})
-		.catch((err) => {
-			console.error('Error connecting to MongoDB:', err);
+			return mongoose;
 		});
+	}
+
+	try {
+		cached.conn = await cached.promise;
+	} catch (e) {
+		cached.promise = null;
+		console.error('Error connecting to MongoDB:', e);
+		throw e;
+	}
+
+	return cached.conn;
 };
 
 module.exports = { connectToDB };
